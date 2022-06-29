b3167ab78cc004abc0e038ca4da030c6b5ed94e5
Hongchao Ji <ji.hongchao@foxmail.com>
2022-06-29 08:40:02 +0800

    Update automs.py

--- AutoMS/automs.py
+++ AutoMS/automs.py
@@ -13,19 +13,19 @@ from AutoMS import peakeval
 def AutoMS(file, min_intensity=500, mass_inv=1, rt_inv=30, length=14, 
            params=(8.5101, 1.6113, 0.1950), min_width = 6):
     
-    peaks, pics = hpic.hpic(file, min_intensity=500, min_snr=1, mass_inv=1, rt_inv=30)
+    peaks, pics = hpic.hpic(file, min_intensity=min_intensity, min_snr=1, mass_inv=1, rt_inv=30)
     scores, mspd_snrs, cnn_output, _, _, _ = peakeval.evaluate_peaks(peaks, pics, length=length, 
                                                                      params=params, min_width = min_width, 
                                                                      cal_snr=False, use_cnn=False)
     scores = np.array(scores)
     scores[scores < 0] = 0
     peaks['score'] = scores
-    return scores
+    return peaks
 
 
 
 if __name__ == '__main__':
     
     file = 'data/600mix_pos.mzML'
-    peaks = AutoMS(file, min_intensity = 5000)
+    peaks = AutoMS(file, min_intensity = 50000)
     
